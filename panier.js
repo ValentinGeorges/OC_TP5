abdb13221b62f128f7354756588ee4f4a377b74b
Valentin Georges <valentingeorges9595@hotmail.com>
2020-06-09 10:49:07 +0200

    Upload V1.4
    Code cleaned

--- panier.js
+++ panier.js
@@ -30,11 +30,6 @@ if (arrayLocalStorage == null) {
         prixTotalElt.textContent = "Prix : " + key.price*compteurArticles + " €";
         let buttonsDiv = document.createElement("div");
         let buttonAdd = document.createElement("button");
-        /* buttonAdd.textContent = "Ajouter un article";
-        buttonAdd.setAttribute("class", "buttonsAdd");
-        let buttonRemove = document.createElement("button");
-        buttonRemove.textContent = "Supprimer un article";
-        buttonRemove.setAttribute("class", "buttonsDelete"); */
         imageDiv.appendChild(imageElt);
         articlesElt.appendChild(imageDiv);
         infosDiv.appendChild(nameElt);
@@ -42,9 +37,6 @@ if (arrayLocalStorage == null) {
         infosDiv.appendChild(numberOfArticles);
         infosDiv.appendChild(prixTotalElt);
         articlesElt.appendChild(infosDiv);
-        /* buttonsDiv.appendChild(buttonAdd);
-        buttonsDiv.appendChild(buttonRemove);
-        articlesElt.appendChild(buttonsDiv); */
         teddiesElt.appendChild(articlesElt);
 
         if (compteurArticles <= 1) {
@@ -199,33 +191,3 @@ if (arrayLocalStorage == null) {
             };
         });
 };
-
-/*
-
-// Création d'un array contenant tous les produits affichés sur la page "Panier"
-const fullCart = [...document.getElementsByTagName("article")];
-
-// Création d'un array contenant les boutons "Supprimer" de chaque produit
-const buttonsDelete = [...document.getElementsByClassName("buttonsDelete")];
-    
-for(let i = 0; i < fullCart.length; i++) {
-// Récupération et stockage dans des variables de l'ID et de la couleur du produit à supprimer
-        const id = fullCart[i].dataset.id;
-        const color = fullCart[i].dataset.color;
-
-        for(let n = 0; n < arrayLocalStorage.length; n++) {
-            if(arrayLocalStorage[n].id == id && arrayLocalStorage[n].color == color) {
-                let deleteIndex = n;
-
-                buttonsDelete[i].addEventListener("click", () => {
-                // On retire l'index du produit à supprimer
-                arrayLocalStorage.splice(deleteIndex, 1);
-        
-                // Puis on renvoie le panier mis à jour au localStorage
-                localStorage.setItem("key", JSON.stringify(arrayLocalStorage));
-                });
-            
-        };
-    };
-};
-*/
